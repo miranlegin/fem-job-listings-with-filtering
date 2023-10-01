16ad934d669c49d44295629317828350472c0169
Miran Legin <miran@ador.hr>
2023-10-01 21:04:09 +0200

    a href to "/"

--- src/Card.js
+++ src/Card.js
@@ -17,36 +17,36 @@ function Card({
   addTagHandler,
 }) {
   return (
-    <div key={id} className="card" data-featured={labelFeatured}>
-      <div className="card__logo">
+    <div key={id} className='card' data-featured={labelFeatured}>
+      <div className='card__logo'>
         <img src={logo} alt={company} />
       </div>
-      <div className="card__data">
-        <div className="card__heading">
-          <div className="card__name">{company}</div>
-          <div className="card__labels">
+      <div className='card__data'>
+        <div className='card__heading'>
+          <div className='card__name'>{company}</div>
+          <div className='card__labels'>
             {labelNew && (
-              <span className="card__label" data-label="new">
+              <span className='card__label' data-label='new'>
                 NEW!
               </span>
             )}
             {labelFeatured && (
-              <span className="card__label" data-label="featured">
+              <span className='card__label' data-label='featured'>
                 FEATURED
               </span>
             )}
           </div>
         </div>
-        <div className="card__title">
-          <a href="#">{position}</a>
+        <div className='card__title'>
+          <a href='/'>{position}</a>
         </div>
-        <div className="card__meta">
+        <div className='card__meta'>
           <span>{postedAt}</span>
           <span>{contract}</span>
           <span>{location}</span>
         </div>
       </div>
-      <div className="card__tags">
+      <div className='card__tags'>
         <Tag name={role} addTagHandler={() => addTagHandler('role', role)} />
         <Tag name={level} addTagHandler={() => addTagHandler('level', level)} />
         {languages.map((language) => (
